8262a29d28026f052767250937a61c185ec48948
Avery Berryman <averyberryman@gmail.com>
2023-03-26 22:28:42 -0700

    finish book and shelf assignments

--- src/book.js
+++ src/book.js
@@ -1,12 +1,50 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return { 
+    name: name,
+    age: age, 
+    pronouns: pronouns,
+  }
+}
+
+function saveReview(content, reviews) {
+  if (reviews.indexOf(content) > -1) {
+    
+  } else {
+    reviews.push(content);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+return bookTitle.length*20;
+}
+
+function writeBook(title, character, genre) {
+var writtenBooks = {
+  title: title,
+  mainCharacter: character,
+  pageCount: calculatePageCount(title),
+  genre: genre
+}
+return writtenBooks
+}
+
+function editBook(editedBook) {
+editedBook.pageCount = editedBook.pageCount * .75;
+
+}
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,46 @@
+function shelfBook(title, shelfGenre) {
+  if (shelfGenre.length < 3) {
+    shelfGenre.unshift(title);
+  }
+}
+
+function unshelfBook(title, shelf) {
+for (var i = 0; i < shelf.length; i++) {
+  if (shelf[i].title === title) {
+    shelf.splice([i], 1)
+  }
+}
+}
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+return titles.join(', ') 
+}
+
+
+function searchShelf(shelf, title) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++)
+  titles.push(shelf[i].title);
+   console.log(titles)
+if (titles.includes(title)) {
+  return true
+} else {
+  return false
+  }
+}
+
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+shelfBook,
+unshelfBook,
+listTitles,
+searchShelf
 };
